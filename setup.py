e8603ec017e299dd671b0a5021d15bf7be6ea825
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-09 18:33:34 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cs118x-grading-script',
+    version='0.0.1',
+    description='Grading script for CS118X',
+    author='payne1778',
+    author_email='56892670+payne1778@users.noreply.github.com ',
+    url='github.com',
+    packages=find_packages(),
+    install_requires=[
+        'types-python-dateutil',
+    ],
+)
